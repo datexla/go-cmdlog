828c725cd495eaa1b21958ffcd072318e685713d
Wsine <weizhy3@hotmail.com>
2016-12-09 13:50:58 +0800

    initial commit

---
+++ cmdlog.go
@@ -0,0 +1,21 @@
+package cmdlog
+
+import (
+	"time"
+	"os/exec"
+)
+
+var (
+	defaultPathToFile = "/var/log/docker/scheduler.log"
+	serviceCome       = "[service come  ]"
+	serviceFinish     = "[service finish]"
+	scorePrint        = "[score   print ]"
+)
+
+func write(state string, event string, pathToFile string) {
+	t := time.Now().Format(time.UnixDate)
+	content := t + "\t" + state + "\t" + event
+	command := "echo \"" + content + "\" >> " + pathToFile
+	cmd := exec.Command("/bin/sh", "-c", command)
+	cmd.Run()
+}
