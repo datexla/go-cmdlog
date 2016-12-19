234865585db095ce603eb34d1eba5b834ffc472d
Wsine <weizhy3@hotmail.com>
2016-12-19 19:23:16 +0800

    updated

--- cmdlog.go
+++ cmdlog.go
@@ -1,20 +1,21 @@
 package cmdlog
 
 import (
-	"time"
 	"os/exec"
 )
 
 const (
 	DefaultPathToFile = "/var/log/docker/scheduler.log"
-	ServiceCome       = "[service come  ]"
-	ServiceFinish     = "[service finish]"
-	ScorePrint        = "[score   print ]"
+	ServiceCome       = "[service  come  ]"
+	ServiceFinish     = "[service  finish]"
+	ScorePrint        = "[score    print ]"
+	ScheduleFinish    = "[schedule finish]"
+	ManagerInfo       = "[Manager  Info  ]"
+	Debug             = "[debug          ]"
 )
 
 func Write(state string, event string, pathToFile string) {
-	t := time.Now().Format(time.UnixDate)
-	content := t + "\t" + state + "\t" + event
+	content := "$(date +'%F %T.%3N') " + state + " " + event
 	command := "echo \"" + content + "\" >> " + pathToFile
 	cmd := exec.Command("/bin/sh", "-c", command)
 	cmd.Run()
